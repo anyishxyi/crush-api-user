b61e06bffbed6075c707c7997c7504abb26d4e01
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-02-17 19:04:43 +0100

    add func test get user info

--- server/test/functional/user.test.js
+++ server/test/functional/user.test.js
@@ -14,23 +14,32 @@ var savedUser = null;
 
 describe("testing-user-routes", () => {
   it("GET /user get all users", async done => {
-    const users = await request.get("/user");
+    const users = await request.get("/user")
 		expect(users.body).toBeDefined()
 		expect(users.status).toBe(200)
 		done()
   });
-  it("Post /user create an user", async done => {
+  it("POST /user create an user", async done => {
     const response = await request.post("/user")
 																	.send(userData);
 		savedUser = response.body ? response.body.userSaved : null
 		expect(response.text).toBeDefined()
 		expect(response.status).toBe(201)
 		expect(savedUser._id).toBeDefined()
-		expect(savedUser.email).toBe(userData.email);
-		expect(savedUser.firstName).toBe(userData.firstName);
-		expect(savedUser.lastName).toBe(userData.lastName);
+		expect(savedUser.email).toBe(userData.email)
+		expect(savedUser.firstName).toBe(userData.firstName)
+		expect(savedUser.lastName).toBe(userData.lastName)
 		done()
   });
+	it('GET /user get user info', async done => {
+		const response = await request.get(`/user/${savedUser._id}`)
+		const userFound = response.body ? response.body : null
+		expect(userFound._id.toString()).toBe(savedUser._id.toString())
+		expect(userFound.email).toBe(savedUser.email)
+		expect(userFound.firstName).toBe(savedUser.firstName)
+		expect(userFound.lastName).toBe(savedUser.lastName)
+		done()
+	});
 });
 
 afterAll( async () => {
