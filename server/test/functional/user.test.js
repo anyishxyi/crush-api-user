280924a1919d7b083aefdfc55a3b4e707432a837
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-02-17 16:24:25 +0100

    add func test for getall users

--- server/test/functional/user.test.js
+++ server/test/functional/user.test.js
@@ -1,29 +1,22 @@
-// const express = require("express"); // import express
-// const userRoutes = require("../../routes/user"); //import file we are testing
+import mongoose from 'mongoose';
 const supertest = require("supertest"); // supertest is a framework that allows to easily test web apis
-// const app = express(); //an instance of an express app, a 'fake' express app
-const app = require("../../app"); //an instance of an express app, a 'fake' express app
-
+import app from '../../app';
 const request = supertest(app);
 
-// app.use("/api-crush-user", userRoutes); //routes
-
-describe("testing-server-routes", () => {
-  it("GET /states - success", async done => {
-    const { res } = await request.get("api-crush-user/user");
-		console.log('res', res)
-		expect(res.status).toBe(200)
-    // expect(res).toEqual([
-		// 	{
-		// 		"_id": "602aee340026fab6a71df1e3",
-		// 		"email": "test10@test.cd",
-		// 		"password": "$2b$10$jWzbZYjLVmE9c2E0MzWc4.sNY8L/kWkOzodzxtgis6xCIb7hqgxYy",
-		// 		"firstName": "firstName",
-		// 		"lastName": "lastName",
-		// 		"created_date": "Mon Feb 15 2021 22:57:08 GMT+0100 (heure normale d’Europe centrale)",
-		// 		"__v": 0
-		// 	}
-    // ]);
-		done();
+describe("testing-user-routes", () => {
+  it("GET /user", async () => {
+    const response = await request.get("/user");
+		expect(response.text).toBeDefined()
+		expect(response.status).toBe(200)
   });
 });
+
+afterAll( async () => {
+	try {
+		// await UserModel.findOneAndDelete({_id: savedUser._id});
+		await mongoose.disconnect()
+		console.log('\nSuccessully disconnected to MongoDB Atlas !\n')
+	} catch (error) {
+		console.error(error);
+	}
+})
